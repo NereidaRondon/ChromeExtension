d362d5ce1cf60de7161afaeefc6b8dcb8c703ff1
Nereida Rondon <97356401+NereidaRondon@users.noreply.github.com>
2022-06-12 19:07:02 -0400

    Added Delete button CSS and HTML

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 let myLeads = [];
-const inputElement = document.getElementById("input-el");
+const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-const ulElement = document.getElementById("ul-element");
+const ulEl = document.getElementById("ul-el");
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
 
 //when that id is clicked, the function is trigered
 inputBtn.addEventListener("click", function () {
-  myLeads.push(inputElement.value);
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
   renderLeads();
-  inputElement.value = "";
 });
+
+//if true make the function equal to the array
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
 
 function renderLeads() {
   //empty string to house the inputs
@@ -16,10 +27,10 @@ function renderLeads() {
   for (let i = 0; i < myLeads.length; i++) {
     // concatenates all of the inputs together into an array
     listItems += `<li>
-                    <a href = '${myLeads[i]}' target = '_blank'> ${myLeads[i]}
-                    </a>
-                  </li>`;
+    <a href = '${myLeads[i]}' target = '_blank'> ${myLeads[i]}
+    </a>
+    </li>`;
   }
   //replaces the html with the whole list of inputs
-  ulElement.innerHTML = listItems;
+  ulEl.innerHTML = listItems;
 }
