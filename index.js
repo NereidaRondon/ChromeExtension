19afa477db41c43cd4353d581e553fa2d212bd75
Nereida Rondon <97356401+NereidaRondon@users.noreply.github.com>
2022-06-11 22:14:07 -0400

    Working js for link list

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-//let myLeads = [];
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = [];
 const inputElement = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulElement = document.getElementById("ul-element");
@@ -7,14 +6,20 @@ const ulElement = document.getElementById("ul-element");
 //when that id is clicked, the function is trigered
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputElement.value);
-  console.log(myLeads)
-})
+  renderLeads();
+  inputElement.value = "";
+});
 
-//consoles the inputs in the array
-for (let i = 0; i < myLeads.length; i++) {
-document.createElement("li")
-  ulElement.innerHTML += "<li>" myLeads[i] + "</li>";
-  //create element
-  //set text content
-  // append to ul
+function renderLeads() {
+  //empty string to house the inputs
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    // concatenates all of the inputs together into an array
+    listItems += `<li>
+                    <a href = '${myLeads[i]}' target = '_blank'>" ${myLeads[i]}
+                    </a>
+                  </li>`;
+  }
+  //replaces the html with the whole list of inputs
+  ulElement.innerHTML = listItems;
 }
