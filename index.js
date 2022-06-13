bc35c3e99e8f6b0020ffdd37f185c9a28497b7e3
Nereida Rondon <97356401+NereidaRondon@users.noreply.github.com>
2022-06-12 23:21:13 -0400

    Working Delete button on double click

--- index.js
+++ index.js
@@ -2,27 +2,31 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-
+const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 console.log(leadsFromLocalStorage);
-
-//when that id is clicked, the function is trigered
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-
-  renderLeads();
-});
-
 //if true make the function equal to the array
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   renderLeads();
 }
 
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear(); //clear local storage
+  myLeads = []; //clear out the array
+  renderLeads(); //clear out the DOM by activating this function which is now using an empty array
+});
+//when Bank button is clicked,
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value); //input is added to array list
+  inputEl.value = ""; //the input box is cleared/emptied
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+  renderLeads();
+});
+
 function renderLeads() {
-  //empty string to house the inputs
+  //start with an empty string to house the inputs
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
     // concatenates all of the inputs together into an array
